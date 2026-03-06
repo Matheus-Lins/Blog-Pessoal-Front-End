0b4b122d9bd86b9bf2afa9aed269ceba5f6f1d4b
Matheus Lins <matheusantonioslins@outlook.com>
2026-03-06 00:10:53 -0300

    fix(Service) - link render

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal.onrender.com'
+  baseURL: 'https://blogpessoal-1tfw.onrender.com'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
